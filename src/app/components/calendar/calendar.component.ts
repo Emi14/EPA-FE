0f1b8ff23e68d42e30abb5aff673052feec8d6f5
DESKTOP-2UBO6EL\catal <catalin_petre2007@yahoo.com>
2019-03-31 14:13:23 +0300

    calendar first try (full calendar primeng)

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  private events: any[];
+  private options: any;
 
   constructor() { }
 
+  //https://www.primefaces.org/primeng/#/fullcalendar
   ngOnInit() {
+    this.events = [
+      {
+          "title": "All Day Event",
+          "start": "2019-04-01"
+      },
+      {
+          "title": "Long Event",
+          "start": "2019-04-07",
+          "end": "2019-04-10"
+      },
+      {
+          "title": "Repeating Event",
+          "start": "2019-04-09T16:00:00"
+      },
+      {
+          "title": "Repeating Event",
+          "start": "2019-04-16T16:00:00"
+      },
+      {
+          "title": "Conference",
+          "start": "2019-04-11",
+          "end": "2019-04-13"
+      }
+    ];
+
+    this.options = {
+      header: {
+          left: 'today,prev,next',
+          right: 'title',
+          center: 'month,agendaWeek,agendaDay'
+      }
+    }
   }
 
 }
