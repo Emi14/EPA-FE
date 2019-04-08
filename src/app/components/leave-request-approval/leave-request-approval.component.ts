4753c87d56ba294e2152f0135c29ec81dcab9233
Catalin Petre <Catalin.Petre@interactivesw.ro>
2019-04-08 15:44:16 +0300

    leave request approval

--- src/app/components/leave-request-approval/leave-request-approval.component.ts
+++ src/app/components/leave-request-approval/leave-request-approval.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-request-approval.component.css']
 })
 export class LeaveRequestApprovalComponent implements OnInit {
+  private usersRequests: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    //get users requests from server (toate care sunt pending)
+    this.usersRequests.push({
+      user: 'user',
+      startDate: 'startDate',
+      endDate: 'endDate',
+      leaveRequestType: 'Vacation'
+    })
+  }
+
+  private rejectRequest(request: any) {
+    //status to rejected/deny -> update pe server
+    //elimina din lista cu usersRequests   this.userRequests = this.userRequest.filter(ur => ur !== request);
+  }
+
+  private acceptRequest(request: any) {
+    //status to accept -> update pe server
+    //elimina din lista cu usersRequests   this.userRequests = this.userRequest.filter(ur => ur !== request);
   }
 
 }
