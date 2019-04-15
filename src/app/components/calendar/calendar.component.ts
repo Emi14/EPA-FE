4ab087e7992811b1fbe9964cf004fc57f235042c
Catalin Petre <Catalin.Petre@interactivesw.ro>
2019-04-15 14:22:24 +0300

    represent accepted request of a user in calendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -12,45 +12,26 @@ export class CalendarComponent implements OnInit {
   private events: any[];
   private options: any;
   private currentUser: User;
-  
+
   constructor(private authenticationService: AuthenticationService, private vacationProviderService: VacationProviderService) { }
 
   //https://www.primefaces.org/primeng/#/fullcalendar
   ngOnInit() {
     this.currentUser = this.authenticationService.currentUserValue;
-    console.warn('currentUser', this.currentUser);
 
-    //ia cocediile aprobate ale utilizatorului curent (get by id)
     this.vacationProviderService.getAllVacationsForAnUser(this.currentUser.id).subscribe(res => {
       let approvedRequests = res.filter(request => request.vacationRequestStatus === 'APPROVED');
       //de convertit astea in formatul unui event
       console.warn('approved requests', approvedRequests);
-    })
-
-    this.events = [
-      {
-          "title": "All Day Event", //la titlu o sa pun tipul concediului
-          "start": "2019-04-01"
-      },
-      {
-          "title": "Long Event",
-          "start": "2019-04-07",
-          "end": "2019-04-10"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2019-04-09"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2019-04-16"
-      },
-      {
-          "title": "Conference",
-          "start": "2019-04-11",
-          "end": "2019-04-13"
+      this.events = [];
+      for (let i=0;i<approvedRequests.length; ++i) {
+        this.events.push({
+          "title": approvedRequests[i].vacationRequestType,
+          "start": approvedRequests[i].startDate,
+          "end": approvedRequests[i].endDate
+        })
       }
-    ];
+    })
 
     this.options = {
       header: {
